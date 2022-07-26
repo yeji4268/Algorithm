c669c9aaf3a760f59d771874f0e84370a12429d6
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:22:32 +0900

    Add files via upload

---
+++ 2292_벌집.py
@@ -0,0 +1,24 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+n = int(input())
+
+nums_pileup = 1  
+cnt = 1
+while n > nums_pileup :
+    nums_pileup += 6 * cnt  
+    cnt += 1  
+print(cnt)
