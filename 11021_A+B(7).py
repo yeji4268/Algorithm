cd774fd03340fb06f423dd6bf842fad4725bd9c5
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:19:03 +0900

    Add files via upload

---
+++ 11021_A+B(7).py
@@ -0,0 +1,22 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+N = int(input())
+
+for i in range(0, N):
+    A, B = map(int, input().split())
+    print(f"Case #{i+1}: {A+B}")
+    
