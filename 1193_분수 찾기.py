8c9a87ace17e0b4fd81e6e6566467a069730f18d
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:34:20 +0900

    Update 1193_분수 찾기.py

--- 1193_분수 찾기.py
+++ 1193_분수 찾기.py
@@ -1,19 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
-# +
 X=int(input())
 
 line=1
@@ -29,6 +13,3 @@ else:
     b=X
     
 print(a, '/', b, sep='')
-# -
-
-
