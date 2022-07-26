a171a9dadf3f34f85974d09f318afcb51e27997a
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:29:46 +0900

    Add files via upload

---
+++ 8393_합.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+N = int(input())
+sum = 0
+for i in range(1, N +1):
+    sum += i
+print(sum)
