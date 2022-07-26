10760103fbd1e6f7573b0051a4bfe1c5981b5527
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:29:22 +0900

    Add files via upload

---
+++ 3052_나머지.py
@@ -0,0 +1,23 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+nums = list()
+for _ in range(10):
+    num = int(input())
+    nums.append(num%42)
+nums = set(nums)
+print(len(nums))
+
+
