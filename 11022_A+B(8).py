faa01b31377cfd57900680becb154bdbb1ac8697
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:33:26 +0900

    Update 11022_A+B(8).py

--- 11022_A+B(8).py
+++ 11022_A+B(8).py
@@ -1,24 +1,5 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
-# +
 N = int(input())
 
 for i in range(N):
     A, B = map(int, input().split())
     print(f"Case #{i + 1}: {A} + {B} = {A+B}")
-# -
-
-
