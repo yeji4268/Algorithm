063d7e6ee5f3bf09b7a87671765f6897c230f502
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:32:16 +0900

    Update 10872_팩토리얼.py

--- 10872_팩토리얼.py
+++ 10872_팩토리얼.py
@@ -1,24 +1,5 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
 N = int(input())
 fac = 1
 for i in range(1, N+1):
     fac = fac * i
 print(fac)
-
-
-
-
