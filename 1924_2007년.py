e396bede1a6d4d16b91afae5b1c25aeafb2ba282
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:21:26 +0900

    Add files via upload

---
+++ 1924_2007년.py
@@ -0,0 +1,26 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+day = 0 
+dayList = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+weekList = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"]
+
+x, y = map(int, input().split())
+
+for i in range(x-1):
+    day = day + dayList[i]
+day = (day + y) % 7
+print(weekList[day])
