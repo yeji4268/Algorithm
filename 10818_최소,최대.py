6a541faa6d5e38c52c45a08af76e5f79ef7b23d9
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:10:53 +0900

    Add files via upload

---
+++ 10818_최소,최대.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+cnt = int(input())
+numbers = list(map(int, input().split()))
+print(min(numbers),max(numbers))
+
+
