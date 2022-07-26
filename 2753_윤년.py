0fe16d30f338d0ed2923f4f716200f6510eda0f7
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:39:19 +0900

    Update 2753_윤년.py

--- 2753_윤년.py
+++ 2753_윤년.py
@@ -1,22 +1,5 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
 year = int(input())
 if(((year % 4 == 0) and (year % 100 != 0)) or (year % 400 == 0)):
     print(1)
 else :
     print(0)
-
-
