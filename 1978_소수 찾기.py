20341178937a4dbaab9926635cec92ebbb957643
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:22:25 +0900

    Add files via upload

---
+++ 1978_소수 찾기.py
@@ -0,0 +1,27 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+N = int(input())
+cnt = 0
+nums = list(map(int, input().split()))
+for num in nums:
+    for i in range(1, num):
+        if(num % i != 0 or num == 1):
+            break 
+        else:
+            cnt = cnt + 1
+print(cnt)
+
+
