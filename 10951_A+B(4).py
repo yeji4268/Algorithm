086e5dcb7cb233ac62196e67eb98a4823d911cb4
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:13:59 +0900

    Add files via upload

---
+++ 10951_A+B(4).py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+while True:
+    try:
+        A, B = map(int, input().split())
+        print(A+B)
+    except:
+        breeak
