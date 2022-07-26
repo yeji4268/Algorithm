adc8e2645356f336a1e2224cdac02d49e0860693
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:39:45 +0900

    Update 2869_달팽이는 올라가고 싶다.py

--- 2869_달팽이는 올라가고 싶다.py
+++ 2869_달팽이는 올라가고 싶다.py
@@ -1,20 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
 a,b,v = map(int,input().split())
 k = (v-b)/(a-b)
 print(int(k) if k == int(k) else int(k)+1)
-
-
