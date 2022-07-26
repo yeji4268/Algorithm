4e7aaadccea6362c5c4b21bd7215ca7e6297166c
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:39:31 +0900

    Update 2775_부녀회장이 될테야.py

--- 2775_부녀회장이 될테야.py
+++ 2775_부녀회장이 될테야.py
@@ -1,19 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
-# +
 t = int(input())
 
 for _ in range(t):  
@@ -23,7 +7,4 @@ for _ in range(t):
     for k in range(floor):  
         for i in range(1, num): 
             f[i] += f[i-1] 
-    print(f[-1]) 
-# -
-
-
+    print(f[-1])
