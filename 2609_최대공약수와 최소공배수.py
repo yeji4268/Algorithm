5bf993104676f02fe06d0722ec32062f72256bf5
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:38:33 +0900

    Update 2609_최대공약수와 최소공배수.py

--- 2609_최대공약수와 최소공배수.py
+++ 2609_최대공약수와 최소공배수.py
@@ -1,19 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
-# +
 from math import gcd
 def lcm(x, y):
     return x * y // gcd(x, y)
@@ -21,7 +5,3 @@ def lcm(x, y):
 a, b = map(int, input().split())
 print(gcd(a, b))
 print(lcm(a, b))
-        
-# -
-
-
