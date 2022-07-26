bbd75539e4f84f87109e5e15456bfd79f7437a13
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:40:33 +0900

    Update 4153_직각삼각형.py

--- 4153_직각삼각형.py
+++ 4153_직각삼각형.py
@@ -1,18 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
 while True:
     a, b, c = map(int, input().split())
     num = [a, b, c]
@@ -24,5 +9,3 @@ while True:
         print("right")
     else:
         print("wrong")
-
-
