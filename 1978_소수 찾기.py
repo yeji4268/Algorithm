d7af1fde97425b7df43f6c5790e460a04304ef36
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:36:26 +0900

    Update 1978_소수 찾기.py

--- 1978_소수 찾기.py
+++ 1978_소수 찾기.py
@@ -1,18 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
 N = int(input())
 cnt = 0
 nums = list(map(int, input().split()))
@@ -23,5 +8,3 @@ for num in nums:
         else:
             cnt = cnt + 1
 print(cnt)
-
-
