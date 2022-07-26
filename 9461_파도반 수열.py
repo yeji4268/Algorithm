e390a0fa57a844177e073718137fbd75719ef16f
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:41:28 +0900

    Update 9461_파도반 수열.py

--- 9461_파도반 수열.py
+++ 9461_파도반 수열.py
@@ -1,18 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
 t = int(input())
 num = [1, 1, 1, 2, 2]
 for i in range(5, 100):
@@ -20,5 +5,3 @@ for i in range(5, 100):
 for _ in range(t):
     n = int(input())
     print(num[n-1])
-
-
