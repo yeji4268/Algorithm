765aa0299ab531e52e337b51bf90049bb2c9987f
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:40:45 +0900

    Update 4344_평균은 넘겠지.py

--- 4344_평균은 넘겠지.py
+++ 4344_평균은 넘겠지.py
@@ -1,19 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
-# +
 C = int(input())
 
 for _ in range(C):
@@ -21,6 +5,3 @@ for _ in range(C):
     avg = sum(nums[1:])/nums[0]
     nums = [x for x in nums if x > avg]
     print(f"{len(nums)/nums[0] * 100:.3f}%")
-# -
-
-
