2b273871c40713232031d47d7e066efb3f56a82e
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:20:03 +0900

    Add files via upload

---
+++ 1193_분수 찾기.py
@@ -0,0 +1,34 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+X=int(input())
+
+line=1
+while X>line:
+    X-=line
+    line+=1
+    
+if line%2==0:
+    a=X
+    b=line-X+1
+else:
+    a=line-X+1
+    b=X
+    
+print(a, '/', b, sep='')
+# -
+
+
