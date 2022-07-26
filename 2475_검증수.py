786c53bc989e7d85e62cc180f7a2c6b9173b5e84
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:36:52 +0900

    Update 2475_검증수.py

--- 2475_검증수.py
+++ 2475_검증수.py
@@ -1,18 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
 a, b, c, d, e = map(int, input().split())
 num = (a**2 + b**2 + c**2 + d**2 + e**2) % 10
 print(num)
