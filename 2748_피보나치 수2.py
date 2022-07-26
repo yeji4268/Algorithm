15eb7a0894ca6a7b36595fea9796e170da154b12
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:28:12 +0900

    Add files via upload

---
+++ 2748_피보나치 수2.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+n = int(input())
+num = [0, 1]
+for i in range(n):
+    num.append(num[i] + num[i + 1])
+print(num[n])
