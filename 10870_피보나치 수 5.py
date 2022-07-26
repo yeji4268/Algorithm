d1d1f75e99f6f173dac06f6cbabe7ec31f3ecb1f
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:32:03 +0900

    Update 10870_피보나치 수 5.py

--- 10870_피보나치 수 5.py
+++ 10870_피보나치 수 5.py
@@ -1,18 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
 N = int(input())
 i = 0 
 nums = [0, 1]
@@ -23,5 +8,3 @@ while cnt != N:
     i += 1
     cnt += 1
 print(nums[N])
-
-
