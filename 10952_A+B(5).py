7a3c0ed902852f551b5f53816a25ca01337aaa5d
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:14:08 +0900

    Add files via upload

---
+++ 10952_A+B(5).py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+while True:
+    A, B = map(int, input().split())
+    if(A == 0 and B == 0):
+        break 
+    print(A + B)
