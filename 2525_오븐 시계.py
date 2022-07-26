cbfb529ce80daee6f2f95fabd884601b1c277e4a
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:37:22 +0900

    Update 2525_오븐 시계.py

--- 2525_오븐 시계.py
+++ 2525_오븐 시계.py
@@ -1,19 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
-# +
 h, m = map(int, input().split())
 time = int(input()) 
 
@@ -27,6 +11,3 @@ if h >= 24:
     h -= 24
 
 print(h, m)
-# -
-
-
