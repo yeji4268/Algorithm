ff9d38eb1d2f123a576810c46447befeb3b579fe
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:34:32 +0900

    Update 1546_평균.py

--- 1546_평균.py
+++ 1546_평균.py
@@ -1,18 +1,3 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.8
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
-
 N = int(input())
 num = list(map(int, input().split()))
 max = max(num)
