bdc72bb8c0afd2e10d7e91a56efae5173b1c1260
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:27:25 +0900

    Add files via upload

---
+++ 2558_A+B(2).py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+A = int(input())
+B = int(input())
+print(A +B)
