d0a79fef804c9b2c30c6cc10d45765aff48343b7
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:28:03 +0900

    Add files via upload

---
+++ 2739_구구단.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+N = int(input())
+for i in range(1, 10):
+    print(f"{N} * {i} = {N * i}")
