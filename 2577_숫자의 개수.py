8cb41208401bdd585438c2216464a03458a7c4a7
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:27:32 +0900

    Add files via upload

---
+++ 2577_숫자의 개수.py
@@ -0,0 +1,32 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+A = int(input())
+B = int(input())
+C = int(input())
+
+num = str(A * B * C)
+
+print(num.count('0'))
+print(num.count('1'))
+print(num.count('2'))
+print(num.count('3'))
+print(num.count('4'))
+print(num.count('5'))
+print(num.count('6'))
+print(num.count('7'))
+print(num.count('8'))
+print(num.count('9'))
