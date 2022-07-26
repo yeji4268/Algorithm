cf3bfcb448e12ff11d405af6321daca3d1b21a70
yeji4268 <89741788+yeji4268@users.noreply.github.com>
2022-07-26 14:20:23 +0900

    Add files via upload

---
+++ 15552_빠른 A+B.py
@@ -0,0 +1,25 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# +
+import sys
+ 
+inp = int(input())
+for i in range(inp):
+        a,b = map(int, sys.stdin.readline().split())
+        print(a+b)
+# -
+
+
